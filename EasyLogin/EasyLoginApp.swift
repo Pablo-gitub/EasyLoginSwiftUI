ac94ac0eabb8c5bd02c520e4f1a24447fb7efe15
Paolo Pietrelli <pablos3339@gmail.com>
2025-03-19 05:19:26 +0100

    Initial Commit

---
+++ EasyLogin/EasyLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyLoginApp.swift
+//  EasyLogin
+//
+//  Created by Paolo Pietrelli on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct EasyLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EasyLoginTests/EasyLoginTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyLoginTests.swift
+//  EasyLoginTests
+//
+//  Created by Paolo Pietrelli on 19/03/25.
+//
+
+import Testing
+@testable import EasyLogin
+
+struct EasyLoginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
